c760a0b301c9d20acad52fdd797464ceb058fd73
junliang liu <850346974ll@gmail.com>
2020-12-09 08:22:50 -0500

    init

---
+++ routes/people.js
@@ -0,0 +1,8 @@
+const {index,show} = require('../controllers/people');
+
+ module.exports = router => {
+    router.get('/people', index);
+    router.get('/people/:id', show);
+
+     return router;
+ };   
